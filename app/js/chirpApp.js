638960def235943e428bd9e7fd4d983eea292be2
arbreton <andrebreton@live.com>
2016-11-09 11:51:07 -0800

    Separated each template into single partials and wired up the templates with angular

--- app/js/chirpApp.js
+++ app/js/chirpApp.js
@@ -1,7 +1,32 @@
-var app = angular.module('chirpApp', []);
+var app = angular.module('chirpApp', ['ngRoute']);
 
+app.config(function($routeProvider){
+  $routeProvider
+    //the timeline display
+    .when('/', {
+      templateUrl: 'main.html',
+      controller: 'mainController'
+    })
+    //the login display
+    .when('/login', {
+      templateUrl: 'login.html',
+      controller: 'authController'
+    })
+    //the signup display
+    .when('/register', {
+      templateUrl: 'register.html',
+      controller: 'authController'
+    });
+});
 app.controller('mainController', function($scope){
-  ...
+  $scope.posts = [];
+  $scope.newPost = {created_by: '', text: '', created_at: ''};
+
+  $scope.post = function(){
+  $scope.newPost.created_at = Date.now();
+  $scope.posts.push($scope.newPost);
+  $scope.newPost = {created_by: '', text: '', created_at: ''};
+};
   };
 });
 app.controller('authController', function($scope){
